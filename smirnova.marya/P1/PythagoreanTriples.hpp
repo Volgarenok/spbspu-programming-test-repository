64eeac728155ea8bf3d0ac5c7938c9818584978c
SmirnovaMarya <unlenmaria@gmail.com>
2026-01-22 19:02:44 +0300

    All

---
+++ smirnova.marya/P1/PythagoreanTriples.hpp
@@ -0,0 +1,20 @@
+#ifndef PYTHAGOREANTRIPLES_HPP
+#define PYTHAGOREANTRIPLES_HPP
+
+#include "ITrait.hpp"
+
+class PythagoreanTriples final : public ITrait {
+public:
+    PythagoreanTriples();
+protected:
+    bool do_add(int value) override;
+public:
+    int value() const override;
+    bool computable() const override;
+private:
+  int a = 0, b = 0, size = 0;
+  int count;
+  bool pyCheck(int x, int y, int z) const;
+};
+
+#endif
