9b2a71e609fb46e31d2d43e36b7ed68eb38cfa25
SmirnovaMarya <unlenmaria@gmail.com>
2026-01-22 18:45:55 +0300

    P1

--- smirnova.marya/P1/MaxIncreasing.cpp
+++ smirnova.marya/P1/MaxIncreasing.cpp
@@ -4,8 +4,8 @@ MaxIncreasing::MaxIncreasing() : last(0), current(0), maximum(0), hasValue(false
 
 bool MaxIncreasing::do_add(int value) {
   if (!hasValue) {
-      last = value; 
-      current = maximum = 1; 
+      last = value;
+      current = maximum = 1;
       hasValue = true;
       return false;
   }
