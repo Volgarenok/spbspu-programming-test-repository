d38e3d948b8ebd0369bf5aebb8d3f3d799e3aa65
SmirnovaMarya <unlenmaria@gmail.com>
2026-01-22 18:53:03 +0300

    All P1

--- smirnova.marya/P1/io.cpp
+++ smirnova.marya/P1/io.cpp
@@ -4,48 +4,55 @@
 
 #include "ITrait.hpp"
 
-int input(ITrait** traits, int traitCount)
+int input(ITrait **traits, int traitCount)
 {
-    int x;
-    int totalAdded = 0;
+  int x;
+  int totalAdded = 0;
 
-    while (std::cin >> x)
+  while (std::cin >> x)
+  {
+    if (x == 0)
     {
-        if (x == 0) {
-            while (std::cin.peek() == ' ' || std::cin.peek() == '\t')
-              std::cin.get();
-            int next = std::cin.peek();
-            if (next == '\n') {
-                if (totalAdded == 0) {
-                  std::cerr << "Error: сannot identify       sequence\n";
-                  return 2;
-                }
-                return 0;
-            }
+      while (std::cin.peek() == ' ' || std::cin.peek() == '\t')
+        std::cin.get();
+      int next = std::cin.peek();
+      if (next == '\n')
+      {
+        if (totalAdded == 0)
+        {
+          std::cerr << "Error: сannot identify       sequence\n";
+          return 2;
         }
-        for (int i = 0; i < traitCount; ++i)
-          traits[i]->add(x);
-        totalAdded++;
+        return 0;
+      }
     }
+    for (int i = 0; i < traitCount; ++i)
+      traits[i]->add(x);
+    totalAdded++;
+  }
 
-		if (std::cin.fail()) {
-      std::cerr << "Error: failed to read data\n";
-      return 1;
-    }
+  if (std::cin.fail())
+  {
+    std::cerr << "Error: failed to read data\n";
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
 
-void output(ITrait** traits, int traitCount)
+void output(ITrait **traits, int traitCount)
 {
-    bool errorOccurred = false;
-    for (int i = 0; i < traitCount; ++i)
+  bool errorOccurred = false;
+  for (int i = 0; i < traitCount; ++i)
+  {
+    if (traits[i] && traits[i]->computable())
     {
-        if (traits[i] && traits[i]->computable()) {
-            std::cout << i + 1 << " characteristic: " << traits[i]->value() << "\n";
-        } else {
-            std::cout << i + 1 << " characteristic: not enough data\n";
-            errorOccurred = true;
-        }
+      std::cout << i + 1 << " characteristic: " << traits[i]->value() << "\n";
     }
+    else
+    {
+      std::cout << i + 1 << " characteristic: not enough data\n";
+      errorOccurred = true;
+    }
+  }
 }
