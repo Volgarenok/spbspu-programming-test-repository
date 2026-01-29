872d05043c65c54d480822530cefbd68a203957a
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-01-30 01:37:02 +0300

    make isZero

--- lachugin.mikhail/P1/main.cpp
+++ lachugin.mikhail/P1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+namespace lachugin
+{
+  bool isZero(int n)
+  {
+    if (n == 0)
+    {
+      return true;
+    }
+    return false;
+  }
+
+
+}
+
 int main()
 {
+  int n = 0;
+
+  int res1;
+  int res2;
 
 }
