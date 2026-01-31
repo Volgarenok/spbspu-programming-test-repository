06eb057d089e0750720b505230db63cffe0ae406
Oleg <n7o.nto@yandex.ru>
2026-01-31 19:38:02 +0300

    Implemented add function

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 
-void append(int *a, int b, size_t &s);
+void append(int *&an, int b, size_t &s);
 
 int main()
 {
-  size_t n = 0;
+  size_t n = 1;
   int * seq = new int[1];
-  int el = -1;
+  int el = 0;
   std::cin>>el;
   seq[0] = el;
   std::cin>>el;
   while (el!=0)
   {
     append(seq, el, n);
+    std::cin>>el;
   }
   if (std::cin.fail())
   {
@@ -24,3 +25,16 @@ int main()
   }
 }
 
+void append(int *&an, int b, size_t &s)
+{
+  int * tempseq = new int[s+1];
+  for (size_t i = 0; i<s; i++)
+  {
+    tempseq[i] = an[i];
+  }
+  tempseq[s] = b;
+  s++;
+  delete[] an;
+  an = tempseq;
+  tempseq = nullptr;
+}
