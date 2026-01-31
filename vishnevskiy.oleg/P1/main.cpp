bb84e9184d3be3b407d4a99c0c3e8f019a039a95
Oleg <n7o.nto@yandex.ru>
2026-01-31 13:44:54 +0300

    Added sequence input

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+void append(int *a, int b, size_t &s);
+
 int main()
 {
-  return 0;
+  size_t n = 0;
+  int seq = new int[1];
+  int el = -1;
+  std::cin>>el;
+  seq[0] = el;
+  std::cin>>el;
+  while (el!=0)
+  {
+    append(seq, el, n);
+  }
+  if (std::cin.fail())
+  {
+    return 1;
+  }
+  if (n==1)
+  {
+    return 1;
+  }
 }
+
