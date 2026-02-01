2d809cea189bcee2bf4dfeb4fb9d8653aceb5ce0
xRonnyn <pashuha.k@gmail.com>
2026-02-02 01:27:38 +0300

    num input

--- karpenkov.pavel/P1/main.cpp
+++ karpenkov.pavel/P1/main.cpp
@@ -2,4 +2,15 @@
 
 int main()
 {
+  int num;
+  while (true) {
+    std::cin >> num;
+    if (std::cin.fail()) {
+      std::cerr << "Invalid input" << '\n';
+      return 1;
+    }
+    if (num == 0) {
+      break;
+    }
+  }
 }
