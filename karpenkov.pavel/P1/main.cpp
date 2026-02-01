d129fc52577576d3954fae6d9acea97f9e153ae2
xRonnyn <pashuha.k@gmail.com>
2026-02-02 02:11:50 +0300

    AFT-MAX

--- karpenkov.pavel/P1/main.cpp
+++ karpenkov.pavel/P1/main.cpp
@@ -2,23 +2,31 @@
 
 int main()
 {
-  int num, previousSign = -1;
-  size_t countNum = 0, changeCount = 0;
+  int number, previousSign = -1;
+  int maxNumber = std::numeric_limits<int>::min();
+  size_t countNum = 0, changeCount = 0, afterMax = 0;
   while (true) {
-    std::cin >> num;
+    std::cin >> number;
     if (std::cin.fail()) {
       std::cerr << "Invalid input" << '\n';
       return 1;
     }
-    if (num == 0) {
+    if (number == 0) {
       break;
     }
     countNum++;
-    int currentSign = (num > 0) ? 1 : -1;
+    if (maxNumber < number) {
+      maxNumber = number;
+      afterMax = 0;
+    } else {
+      afterMax++;
+    }
+    int currentSign = (number > 0) ? 1 : -1;
     if (countNum > 1 && currentSign != previousSign) {
       changeCount++;
     }
     previousSign = currentSign;
   }
   std::cout << changeCount << '\n';
+  std::cout << afterMax << '\n';
 }
