cd241cf5c0ff1c635c0dffb1b115d8c298fb90ae
Oleg <n7o.nto@yandex.ru>
2026-02-02 10:59:05 +0300

    Reworked code to fit task requirements

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -1,96 +1,69 @@
 #include <iostream>
 
-void append(int *&an, int b, size_t &s);
-int monDec(int *a, size_t s);
-int grtLss(int *a, size_t s);
+bool grtLss(int first, int second, int third);
 
 int main()
 {
-  size_t n = 0;
-  int * seq = new int[1];
-  int el = 0;
-  std::cin >> el;
-  while (el != 0)
+  int currEl = 0, secondEl = 0, firstEl = 0, grt = 0, mon = 0, currMon = 1;
+  std::cin >> currEl;
+  if (currEl == 0)
   {
-    try
+    std::cerr << "Error : sequence is too short\n";
+    return 2;
+  }
+  secondEl = currEl;
+  std::cin >> currEl;
+  if (currEl == 0)
+  {
+    std::cout << 1 << "\n";
+    std::cerr << "Error : sequence is too short\n";
+    return 2;
+  }
+  firstEl = secondEl;
+  secondEl = currEl;
+  std::cin >> currEl;
+  if (currEl == 0)
+  {
+    if (firstEl >= secondEl)
     {
-      append(seq, el, n);
+      std::cout << 2 << "\n";
     }
-    catch (...)
+    else
     {
-      std::cerr << "Caught an exception\n";
+      std::cout << 1 << "\n";
     }
-    std::cin >> el;
+    std::cerr << "Error : sequence is too short\n";
+    return 2;
+  }
+  while (currEl != 0)
+  {
+    firstEl = secondEl;
+    secondEl = currEl;
+    std::cin >> currEl;
+    grt = grt+grtLss(firstEl, secondEl, currEl);
+    if (secondEl >= currEl)
+    {
+      currMon++;
+    }
+    else
+    {
+      if (currMon > mon)
+      {
+        mon = currMon;
+      }
+      currMon = 1;
+    }
     if (std::cin.fail())
     {
       std::cerr << "Input failed\n";
       return 1;
     }
   }
-  if (n > 1)
+  if (currMon > mon)
   {
-    std::cout << grtLss(seq, n) <<  " " << monDec(seq, n) << "\n";
-    return 0;
+    mon = currMon;
   }
-  else
-  {
-    if (n == 1)
-    {
-      std::cout << monDec(seq, n) << "\n";
-    }
-    std::cerr << "Error : sequence too short\n";
-    return 2;
-  }
+  std::cout << grt << " " << mon << "\n";
+  return 0;
 }
 
-void append(int *&an, int b, size_t &s)
-{
-  int * tempseq = new int[s+1];
-  for (size_t i = 0; i < s; i++)
-  {
-    tempseq[i] = an[i];
-  }
-  tempseq[s] = b;
-  s++;
-  delete[] an;
-  an = tempseq;
-  tempseq = nullptr;
-}
-
-int grtLss(int *a, size_t s)
-{
-  int res = 0;
-  for (size_t i = 1; i < (s - 1); i++)
-  {
-    if (a[i] < a[i - 1] && a[i] > a[i + 1])
-    {
-      res++;
-    }
-  }
-  return res;
-}
-
-int monDec(int *a, size_t s)
-{
-  int res = 0, currRes = 1;
-  for (size_t i = 1; i < s; i++)
-  {
-    if (a[i - 1] >= a[i])
-    {
-      currRes++;
-    }
-    else
-    {
-      if (currRes > res)
-      {
-        res = currRes;
-      }
-      currRes = 1;
-    }
-  }
-  if (currRes > res)
-  {
-    res = currRes;
-  }
-  return res;
-}
