0dc1fb96be92decaccda8840f766a238a89f3321
khalii21 <miratkhalikov007@gmail.com>
2026-02-03 08:16:07 +0500

    struct GrtLss is added

--- khalikov.mirat/P1/main.cpp
+++ khalikov.mirat/P1/main.cpp
@@ -48,13 +48,62 @@ struct LocMin : ITrait
     int temp_;
 };
 
+struct GrtLss : ITrait
+{
+  GrtLss()
+  {
+    k_ = 0;
+    prev_ = 0;
+    next_ = 0;
+    act_ = 0;
+    firstadd_ = true;
+  }
+
+  void operator()(int a)
+  {
+    if (firstadd_)
+    {
+      prev_ = a;
+      act_ = a;
+      next_ = a;
+      firstadd_ = false;
+    }
+    else
+    {
+      prev_ = act_;
+      act_ = next_;
+      next_ = a;
+      if ((act_ > next_) && (act_ < prev_))
+      {
+        k_++;
+      }
+    }
+  }
+
+  int operator()()
+  {
+    return k_;
+  }
+
+  private:
+    bool firstadd_;
+    int k_;
+    int prev_;
+    int act_;
+    int next_;
+};
+
+
 int main()
 {
   int number = 0;
   LocMin a;
+  GrtLss b;
   while(std::cin >> number)
   {
     a(number);
+    b(number);
   }
   std::cout << a() << "\n";
+  std::cout << b() << "\n";
 }
