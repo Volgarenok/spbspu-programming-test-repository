c5880006edff5b1d03b8e1db7380daca75f90d24
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-03 15:49:43 +0300

    add basic functional

--- novikov.misha/P1/main.cpp
+++ novikov.misha/P1/main.cpp
@@ -1,2 +1,43 @@
+#include <iostream>
+namespace novikov
+{
+bool tryReadInteger(int& output){
+    std::cin >> output;
+    return !std::cin.fail();
+}
+void printError(const char* message){
+    std::cerr << message << "\n";
+}
+}
 int main()
-{}
+{
+    using namespace novikov;
+
+    const char* const errorInvalidInput = "Ошибка: Входные данные содержат нечисловые значения";
+    const char* const errorNoZero = "Ошибка: Не было введено число 0 для завершения";
+    std::size_t totalNumbers = 0;
+    bool foundZero = false;
+    int currentNumber = 0;
+
+    while (true) {
+      if (!tryReadInteger(currentNumber)) {
+        std::cerr << errorInvalidInput << '\n';
+        return 1;
+      }
+
+      if (currentNumber == 0) {
+        foundZero = true;
+        break;
+      }
+
+      ++totalNumbers;
+    }
+
+    if (!foundZero) {
+      std::cerr << errorNoZero << '\n';
+      return 1;
+    }
+
+    return 0;
+
+}
