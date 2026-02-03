842c7285ad30b24821ec09282483e1a8b7393e90
L1sa620 <lisevbom@gmail.com>
2026-02-03 20:57:44 +0300

    Error handling

--- matveev.borislav/P1/main.cpp
+++ matveev.borislav/P1/main.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 int main()
 {
-  int a=0, count_max=0, max_num=0, previous=0, count_rem=0;
+  int a = 0, count_max = 0, max_num = 0, previous = 0, count_rem = 0, len=0;
+
   std::cin >> a;
-  max_num=a;
-  if(a == 0)
+  if (std::cin.fail())
   {
+    std::cerr << "Error input" << std::endl;
+    return 1;
+  }
+  max_num = a;
+  if (a == 0)
+  {
+    std::cerr << "No numbers" << std::endl;
     return 2;
   }
-  while(a != 0)
+  while (a != 0)
   {
-    if(a == max_num)
+    len++;
+    if (a == max_num)
     {
       count_max++;
     }
-    if(a > max_num)
+    if (a > max_num)
     {
-      max_num=a;
-      count_max=1;
+      max_num = a;
+      count_max = 1;
     }
 
-    if(previous != 0 && a % previous == 0)
+    if (previous != 0 && a % previous == 0)
     {
       count_rem++;
     }
-    previous=a;
+    previous = a;
     std::cin >> a;
+    if (std::cin.fail())
+    {
+      std::cerr << "Error input" << std::endl;
+      return 1;
+    }
   }
-  if(count_rem == 0)
+  if (len < 2)
   {
     std::cout << "CNT-MAX " << count_max << std::endl;
+    std::cerr << "Error numbers" << std::endl;
     return 2;
   }
   std::cout << "CNT-MAX " << count_max << std::endl;
