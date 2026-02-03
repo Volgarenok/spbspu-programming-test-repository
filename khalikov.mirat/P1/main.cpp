fb6f308431a657ca27587deb447002295e100d70
khalii21 <miratkhalikov007@gmail.com>
2026-02-03 07:53:51 +0500

    struct LocMin is added

--- khalikov.mirat/P1/main.cpp
+++ khalikov.mirat/P1/main.cpp
@@ -3,9 +3,58 @@
 struct ITrait
 {
   virtual void operator()(int a) = 0;
-  virtual size_t operator()() = 0;
+  virtual int operator()() = 0;
   virtual ~ITrait() = default;
 };
 
+struct LocMin : ITrait
+{
+  LocMin()
+  {
+    k_ = 0;
+    min_ = 0;
+    temp_ = 0;
+    firstadd_ = true;
+  }
+
+  void operator()(int a)
+  {
+    if (firstadd_)
+    {
+      min_ = a;
+      k_++;
+      firstadd_ = false;
+    }
+    else
+    {
+      temp_ = a;
+      k_++;
+      if (min_ > temp_)
+      {
+        min_ = temp_;
+      }
+    }
+  }
+
+  int operator()()
+  {
+    return min_;
+  }
+
+  private:
+    bool firstadd_;
+    size_t k_;
+    int min_;
+    int temp_;
+};
+
 int main()
-{}
+{
+  int number = 0;
+  LocMin a;
+  while(std::cin >> number)
+  {
+    a(number);
+  }
+  std::cout << a() << "\n";
+}
