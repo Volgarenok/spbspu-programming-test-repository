0f7abd935c060e667fec1031f7793052f97afe02
khalii21 <miratkhalikov007@gmail.com>
2026-02-05 01:09:33 +0500

    namespace is added

--- khalikov.mirat/P1/main.cpp
+++ khalikov.mirat/P1/main.cpp
@@ -1,127 +1,130 @@
 #include <iostream>
 
-struct ITrait
+namespace khalikov
 {
-  virtual void operator()(int a) = 0;
-  virtual int operator()() = 0;
-  virtual ~ITrait() = default;
-};
+  struct ITrait
+  {
+    virtual void operator()(int a) = 0;
+    virtual int operator()() = 0;
+    virtual ~ITrait() = default;
+  };
 
-struct Size : ITrait
-{
-  Size()
+  struct Size : ITrait
   {
-    k_ = 0;
-  }
-  void operator()(int a)
-  {
-    k_++;
-  }
-  int operator()()
-  {
-    return k_;
-  }
-  private:
-    int k_;
-};
-
-struct LocMin : ITrait
-{
-  LocMin()
-  {
-    k_ = 0;
-    prev_ = 0;
-    next_ = 0;
-    act_ = 0;
-    firstadd_ = true;
-  }
-
-  void operator()(int a)
-  {
-    if (firstadd_)
+    Size()
     {
-      prev_ = a;
-      act_ = a;
-      next_ = a;
-      firstadd_ = false;
+      k_ = 0;
     }
-    else
+    void operator()(int a)
     {
-      prev_ = act_;
-      act_ = next_;
-      next_ = a;
-      if ((act_ < next_) && (act_ < prev_))
+      k_++;
+    }
+    int operator()()
+    {
+      return k_;
+    }
+    private:
+      int k_;
+  };
+
+  struct LocMin : ITrait
+  {
+    LocMin()
+    {
+      k_ = 0;
+      prev_ = 0;
+      next_ = 0;
+      act_ = 0;
+      firstadd_ = true;
+    }
+
+    void operator()(int a)
+    {
+      if (firstadd_)
       {
-        k_++;
+        prev_ = a;
+        act_ = a;
+        next_ = a;
+        firstadd_ = false;
+      }
+      else
+      {
+        prev_ = act_;
+        act_ = next_;
+        next_ = a;
+        if ((act_ < next_) && (act_ < prev_))
+        {
+          k_++;
+        }
       }
     }
-  }
 
-  int operator()()
-  {
-    return k_;
-  }
-
-  private:
-    bool firstadd_;
-    int k_;
-    int prev_;
-    int act_;
-    int next_;
-};
-
-struct GrtLss : ITrait
-{
-  GrtLss()
-  {
-    k_ = 0;
-    prev_ = 0;
-    next_ = 0;
-    act_ = 0;
-    firstadd_ = true;
-  }
-
-  void operator()(int a)
-  {
-    if (firstadd_)
+    int operator()()
     {
-      prev_ = a;
-      act_ = a;
-      next_ = a;
-      firstadd_ = false;
+      return k_;
     }
-    else
+
+    private:
+      bool firstadd_;
+      int k_;
+      int prev_;
+      int act_;
+      int next_;
+  };
+
+  struct GrtLss : ITrait
+  {
+    GrtLss()
     {
-      prev_ = act_;
-      act_ = next_;
-      next_ = a;
-      if ((act_ > next_) && (act_ < prev_))
+      k_ = 0;
+      prev_ = 0;
+      next_ = 0;
+      act_ = 0;
+      firstadd_ = true;
+    }
+
+    void operator()(int a)
+    {
+      if (firstadd_)
       {
-        k_++;
+        prev_ = a;
+        act_ = a;
+        next_ = a;
+        firstadd_ = false;
+      }
+      else
+      {
+        prev_ = act_;
+        act_ = next_;
+        next_ = a;
+        if ((act_ > next_) && (act_ < prev_))
+        {
+          k_++;
+        }
       }
     }
-  }
 
-  int operator()()
-  {
+    int operator()()
+    {
     return k_;
-  }
+    }
 
-  private:
-    bool firstadd_;
-    int k_;
-    int prev_;
-    int act_;
-    int next_;
-};
-
+    private:
+      bool firstadd_;
+      int k_;
+      int prev_;
+      int act_;
+      int next_;
+  };
+}
 
 int main()
 {
+  namespace kh = khalikov;
   int number = 0;
-  LocMin a;
-  GrtLss b;
-  Size c;
+  kh::LocMin a;
+  kh::GrtLss b;
+  kh::Size c;
   while(std::cin >> number)
   {
     a(number);
