78b49aff4944aea71d6d8e783ce354dc48f73d9d
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-06 14:33:00 +0300

    fixed CG 11 for P1

--- novikov.misha/P1/main.cpp
+++ novikov.misha/P1/main.cpp
@@ -2,96 +2,96 @@
 namespace novikov
 {
 bool tryReadInteger(int& output){
-    std::cin >> output;
-    return !std::cin.fail();
+  std::cin >> output;
+  return !std::cin.fail();
 }
 void printError(const char* message){
-    std::cerr << message << "\n";
+  std::cerr << message << "\n";
 }
 }
 int main()
 {
-    using namespace novikov;
+  using namespace novikov;
 
-    const char* const errorInvalidInput = "Ошибка: входные данные содержат нечисловые значения";
-    const char* const errorNoZero = "Ошибка: не было введено число 0 для завершения";
-    const char* const errorIncSeqShort = "Ошибка: последовательность слишком короткая";
-    const char* const errorEvnCntShort = "Ошибка: Последовательность слишком короткая";
+  const char* const errorInvalidInput = "Ошибка: входные данные содержат нечисловые значения";
+  const char* const errorNoZero = "Ошибка: не было введено число 0 для завершения";
+  const char* const errorIncSeqShort = "Ошибка: последовательность слишком короткая";
+  const char* const errorEvnCntShort = "Ошибка: Последовательность слишком короткая";
 
-    size_t maxEvenChain = 0;
-    size_t currentEvenChain = 0;
-    size_t biggerCount = 0;
-    size_t totalNumbers = 0;
-    bool isFirst = true;
-    bool hasError = false;
-    bool foundZero = false;
-    int currentNumber = 0;
-    int previousNumber = 0;
+  size_t maxEvenChain = 0;
+  size_t currentEvenChain = 0;
+  size_t biggerCount = 0;
+  size_t totalNumbers = 0;
+  bool isFirst = true;
+  bool hasError = false;
+  bool foundZero = false;
+  int currentNumber = 0;
+  int previousNumber = 0;
 
-    while (true) {
-      if (!tryReadInteger(currentNumber)) {
-        std::cerr << errorInvalidInput << "\n";
-        return 1;
-      }
-
-      if (currentNumber == 0) {
-        foundZero = true;
-        break;
-      }
-
-      ++totalNumbers;
-
-      if (!isFirst) {
-        if (currentNumber > previousNumber) {
-          ++biggerCount;
-        }
-      } else {
-        isFirst = false;
-      }
-
-      previousNumber = currentNumber;
-    }
-
-    if (currentNumber % 2 == 0) {
-        ++currentEvenChain;
-        if (currentEvenChain > maxEvenChain) {
-          maxEvenChain = currentEvenChain;
-        }
-      } else {
-        currentEvenChain = 0;
-      }
-    }
-
-    if (!foundZero) {
-      std::cerr << errorNoZero << "\n";
+  while (true) {
+    if (!tryReadInteger(currentNumber)) {
+      std::cerr << errorInvalidInput << "\n";
       return 1;
     }
 
-    if (totalNumbers == 0) {
-      std::cout << "0\n";
-      return 0;
+    if (currentNumber == 0) {
+      foundZero = true;
+      break;
     }
 
-    if (totalNumbers >= 2) {
-      std::cout << biggerCount << "\n";
+    ++totalNumbers;
+
+    if (!isFirst) {
+      if (currentNumber > previousNumber) {
+        ++biggerCount;
+      }
     } else {
-      std::cout << "0\n";
-      printError(errorIncSeqShort);
-      hasError = true;
+      isFirst = false;
     }
 
-    if (totalNumbers >= 1) {
-      std::cout << maxEvenChain << "\n";
+    previousNumber = currentNumber;
+  }
+
+  if (currentNumber % 2 == 0) {
+      ++currentEvenChain;
+      if (currentEvenChain > maxEvenChain) {
+        maxEvenChain = currentEvenChain;
+      }
     } else {
-      std::cout << "0\n";
-      printError(errorEvnCntShort);
-      hasError = true;
+      currentEvenChain = 0;
     }
+  }
 
-    if (hasError) {
-      return 2;
-    }
+  if (!foundZero) {
+    std::cerr << errorNoZero << "\n";
+    return 1;
+  }
 
+  if (totalNumbers == 0) {
+    std::cout << "0\n";
     return 0;
+  }
+
+  if (totalNumbers >= 2) {
+    std::cout << biggerCount << "\n";
+  } else {
+    std::cout << "0\n";
+    printError(errorIncSeqShort);
+    hasError = true;
+  }
+
+  if (totalNumbers >= 1) {
+    std::cout << maxEvenChain << "\n";
+  } else {
+    std::cout << "0\n";
+    printError(errorEvnCntShort);
+    hasError = true;
+  }
+
+  if (hasError) {
+    return 2;
+  }
+
+  return 0;
 
 }
