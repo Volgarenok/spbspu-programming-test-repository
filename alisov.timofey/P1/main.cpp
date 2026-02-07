d13fcbb019fea894708d524890602915e63c5ae2
AlisovTimofey <timofeyalisov@gmail.com>
2026-02-07 19:43:02 +0300

    rewrite err + finish EVN-CNT

--- alisov.timofey/P1/main.cpp
+++ alisov.timofey/P1/main.cpp
@@ -2,16 +2,15 @@
 
 int main()
 {
-  int x;
+  int x = 0;
+  int total = 0;
+
+  int maxk = 0;
+  int k = 0;
   if (!(std::cin >> x))
   {
     std::cerr << "Not a number\n";
     return 1;
-  }
-  if (x == 0)
-  {
-    std::cerr << "Eempty sequence\n";
-    return 2;
   }
   while (x != 0)
   {
@@ -20,5 +19,22 @@ int main()
       std::cerr << "Not a number\n";
       return 1;
     }
+    total += 1;
+    if (x % 2 == 0)
+    {
+      k += 1;
+      if (k > maxk)
+      {
+        maxk == k;
+      }
+    }
+    else
+    {
+      k == 0;
+    }
+  }
+  if (total < 2)
+  {
+    std::cerr << "Cant calculate";
   }
 }
