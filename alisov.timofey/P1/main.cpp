e10b13946da73f858154d9f494f83e8ccca06b16
AlisovTimofey <timofeyalisov@gmail.com>
2026-02-07 19:08:35 +0300

    return codes

--- alisov.timofey/P1/main.cpp
+++ alisov.timofey/P1/main.cpp
@@ -2,4 +2,23 @@
 
 int main()
 {
+  int x;
+  if (!(std::cin >> x))
+  {
+    std::cerr << "Not a number\n";
+    return 1;
+  }
+  if (x == 0)
+  {
+    std::cerr << "Eempty sequence\n";
+    return 2;
+  }
+  while (x != 0)
+  {
+    if (!(std::cin >> x))
+    {
+      std::cerr << "Not a number\n";
+      return 1;
+    }
+  }
 }
