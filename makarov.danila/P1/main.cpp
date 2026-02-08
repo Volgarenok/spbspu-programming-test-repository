e7a91922ac869365902d52cf1e81c0bbd897ef2e
Daniel <d.m-2006mail.ru@Mac.dlink>
2026-02-09 02:00:58 +0300

    input and main task

--- makarov.danila/P1/main.cpp
+++ makarov.danila/P1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <limits>
 
 int main() 
 {
+  int tnum, mnum, m2num = std::numeric_limits<int>::min();
+  size_t count = 0;
+  while (true)
+  {
+    std::cin >> tnum;
+    if (tnum == 0) {
+      break;
+    }
+    if (tnum > mnum) {
+      m2num = mnum;
+      mnum = tnum;
+    }
+  }
+  std::cout << m2num << "\n";
   
 }
