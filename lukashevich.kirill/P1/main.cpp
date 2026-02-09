3cabfb61589317d81f28d225c344f1253fca82cf
kirusha-13 <kirillukashevich2007@gmail.com>
2026-02-10 00:02:57 +0300

    Add aft_max(checkout for error with exit code 2 and calculation aft_max)

--- lukashevich.kirill/P1/main.cpp
+++ lukashevich.kirill/P1/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <climits>
 
 namespace lukashevich
 {
   int process()
   {
-    int num, prev_num, sgn_chg = 0;
+    int num = 0, prev_num = 0, sgn_chg = 0, count = 0;
     bool first_num = true;
+    int max_num = INT_MIN;
+    int max_pos = -1;
 
     while (true)
     {
@@ -32,13 +35,39 @@ namespace lukashevich
         first_num = false;
       }
 
+      if (count == 0 || num > max_num)
+      {
+        max_num = num;
+        max_pos = count;
+      }
+
       prev_num = num;
+      ++count;
+    }
+
+    bool aft_max_val = true;
+    if (count == 0)
+    {
+      aft_max_val = false;
+      std::cerr << "error: sequence = 0, aft_max cant be counted\n";
+    }
+
+    if (aft_max_val)
+    {
+      int aft_max = count - max_pos - 1;
+      std::cout << aft_max << "\n";
     }
 
     std::cout << sgn_chg << "\n";
+
+    if (!aft_max_val)
+    {
+      return 2;
+    }
     return 0;
   }
 }
+
 int main()
 {
   return lukashevich::process();
