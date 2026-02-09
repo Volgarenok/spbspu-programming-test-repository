f3a1fc31787121ccad703d03b80bbed311d492a6
Daniel <d.m-2006mail.ru@Mac.dlink>
2026-02-09 03:24:03 +0300

    fix file

--- makarov.danila/P1/main.cpp
+++ makarov.danila/P1/main.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <limits>
 
-int main() 
-{
+int main() {
   int tnum = std::numeric_limits<int>::min(); 
   int mnum = std::numeric_limits<int>::min(); 
   int m2num = std::numeric_limits<int>::min();
   size_t count = 0;
-  while (true)
-  {
+  while (true) {
     std::cin >> tnum;
     if (std::cin.fail()) {
       std::cerr << "Invalid input" << "\n";
       return 1;
     }
-    count++;
     if (tnum == 0) {
       break;
     }
+    count++;
     if (tnum > mnum) {
       m2num = mnum;
       mnum = tnum;
     }
   }
-  if (count <= 2) {
+  if (count <= 1) {
     std::cerr << "Not enough numbers for the task" << "\n";
     return 2;
   }
