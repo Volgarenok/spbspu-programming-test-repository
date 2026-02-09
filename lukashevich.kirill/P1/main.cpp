39cfb663b7570f342a1ffcd2eaed87522ef106d4
kirusha-13 <kirillukashevich2007@gmail.com>
2026-02-09 19:07:34 +0300

    Add check for input and stop input when 0

--- lukashevich.kirill/P1/main.cpp
+++ lukashevich.kirill/P1/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+namespace lukashevich
+{
+  int process()
+  {
+    int num, prev_num, sgn_chg = 0;
+    bool first_num = true;
+
+    while (true)
+    {
+      if (!(std::cin >> num)
+      {
+        std::cerr << "error: input isnt number\n";
+        return 1;
+      }
+      if (num == 0)
+      {
+        break;
+      }
+    }
+  }
+}
 int main()
 {
   return 0;
