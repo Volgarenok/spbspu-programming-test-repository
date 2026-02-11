bef26b2d5717e79e6ff7ec1980cc1eb570fde248
uncharted-bread <gordeychikmika@gmail.com>
2026-02-11 11:12:50 +0300

    added input reading and error handling

--- gordejchik.mikhail/P1/main.cpp
+++ gordejchik.mikhail/P1/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+namespace gordejchik
+{
+  struct ErrorStatus
+  {
+    bool foundError;
+    bool foundZero;
+  };
+
+  bool readInteger(int& output)
+  {
+    std::cin >> output;
+    return !std::cin.fail();
+  }
+
+  void printError(const char* message, ErrorStatus& error)
+  {
+    std::cerr << message << '\n';
+    error.foundError = true;
+  }
+}
+
 int main()
 {
-	return 0;
+  gordejchik::ErrorStatus error{};
+  error.foundError = false;
+  error.foundZero = false;
+
+  return 0;
 }
