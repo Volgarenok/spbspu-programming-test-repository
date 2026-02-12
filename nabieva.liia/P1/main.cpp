5c05dd7a8ee17a5407bfb00ea3d1ea94f11375da
liia <kirilich_lubit@rambler.ru>
2026-02-12 20:15:25 +0500

    implement find maxCount

--- nabieva.liia/P1/main.cpp
+++ nabieva.liia/P1/main.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 //CNT-MAX MON-DEC
 
+struct maxCount {
+  maxCount() : countMax_(0), hasElement_(false) {}
+  void upd(int value) {
+    if (!hasElement_) {
+      hasElement_ = true;
+      maxValue_ = value;
+      countMax_ = 1;
+    }
+    else if (value > maxValue_) {
+      maxValue_ = value;
+      countMax_ = 1;
+    }
+    else if (value == maxValue_) {
+      ++countMax_;
+    }
+  }
+  size_t result() const {
+    return countMax_;
+  }
+private:
+  int maxValue_;
+  size_t countMax_;
+  bool hasElement_;
+};
+
 int main()
 {
   int a = 0;
+  maxCount maxCounter;
   while (std::cin >> a) {
     if (a == 0) {
       break;
     }
+    maxCounter.upd(a);
   }
   if (!std::cin.eof() && std::cin.fail()) {
     std::cerr << "Error input";
     return 1;
   }
+  std::cout << maxCounter.result() << "\n";
 }
