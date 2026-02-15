183f1139dfb1e43a5a6c8314307bcdcf37d64592
Oleg <n7o.nto@yandex.ru>
2026-02-15 21:13:34 +0300

    Implemented class methods

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -7,9 +7,9 @@ struct grtLss {
     first_ = second_;
     second_ = third_;
     third_ = a;
-    if (f < 2)
+    if (f_ < 2)
     {
-      f++;
+      f_++;
     }
     else
     {
@@ -23,6 +23,23 @@ struct grtLss {
   {
     return count_;
   }
+  bool operator()(size_t &error)
+  {
+    if (f_ < 2)
+    {
+      if (error == 0) {
+        error = 2;
+      }
+      std::cerr << "Error : sequence is too short\n";
+      return true;
+    }
+    else
+    {
+      error = 0;
+      return false;
+    }
+    return false;
+  }
  private:
   size_t count_;
   int first_, second_, third_, f_;
@@ -34,23 +51,48 @@ struct monDec {
   {
     prev_ = curr_;
     curr_ = a;
-    if (f == 0)
+    if (f_ == 0)
     {
       tempCount_ = 1;
-      f = 1;
+      f_ = 1;
     }
     else
     {
+      f_ = 2;
+      if (prev_ >= curr_)
+      {
+        tempCount_++;
+      }
+      else
+      {
+        tempCount_ = 1;
+      }
       if (tempCount_ > count_)
       {
         count_ = tempCount_;
       }
-      tempCount_ = 0;
     }
   }
   size_t operator()() const
   {
     return count_;
+  }
+  bool operator()(size_t &error)
+  {
+    if (f_ != 2)
+    { 
+      if (error == 0){
+        error = 2;
+      }
+      std::cerr << "Error : sequence is too short\n";
+      return true;
+    }
+    else
+    {
+      error = 0;
+      return false;
+    }
+    return false;
   }
  private:
   size_t count_, tempCount_;
@@ -60,20 +102,30 @@ struct monDec {
 int main()
 {
   int el = 0;
+  size_t error = 0;
   grtLss g;
   monDec m;
-  while (std::cin >> el)
+  int cn = 0;
+  while (std::cin >> el && el != 0)
   {
     g(el);
     m(el);
   }
-  if (!std::cin.eof())
+  if (std::cin.fail())
   {
     std::cerr << "Input failed\n";
     return 1;
   }
-  std::cout << g() << " " << m() << "\n";
-  return 0;
+  if (!g(error))
+  {
+    std::cout << g() << " ";
+  }
+  if (!m(error))
+  {
+    std::cout << m() << "a";
+  }
+  std::cout << "\n";
+  return error;
 }
 
 
