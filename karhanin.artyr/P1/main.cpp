a164d7e9e28fc523a1c1b2b82b73751a4892d192
sftuuu <artyr27092007@gmail.com>
2026-02-15 23:25:51 +0300

    Fix P1

--- karhanin.artyr/P1/main.cpp
+++ karhanin.artyr/P1/main.cpp
@@ -1,38 +1,37 @@
 #include <vector>
 #include <iostream>
 
-int count_increasing(const int* numbers, std::size_t size) 
+int count_increasing(const int* numbers, std::size_t size)
 {
-    if (size <= 1) {
-        return 0;
-    }
-    int count = 0;
-
-    for (std::size_t i = 1; i < size; i++) 
+  if (size <= 1)
+  {
+    return 0;
+  }
+  int count = 0;
+  for (std::size_t i = 1; i < size; i++)
+  {
+    if (numbers[i] > numbers[i - 1])
     {
-        if (numbers[i] > numbers[i - 1]) 
-        {
-            count++;
-        }
+      count++;
     }
-
-    return count;
+  }
+  return count;
 }
 
 
-int main() 
+int main()
 {
-    std::vector <int> numbers;
-    int num;
-    while (std::cin >> num && num != 0) 
-    {
-        numbers.push_back(num);
-    }
-    if (std::cin.fail() && !std::cin.eof()) 
-    {
-        std::cerr << "Input error";
-       return 1;
-    }
-    std::cout << count_increasing(numbers.data(), numbers.size());
-    return 0;
+  std::vector <int> numbers;
+  int num;
+  while (std::cin >> num && num != 0)
+  {
+    numbers.push_back(num);
+  }
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "Input error";
+    return 1;
+  }
+  std::cout << count_increasing(numbers.data(), numbers.size());
+  return 0;
 }
