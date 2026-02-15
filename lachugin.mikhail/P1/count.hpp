a179861048e2923f2a4c22d081183fc16dacee29
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-02-15 20:22:10 +0300

    make count.hpp

---
+++ lachugin.mikhail/P1/count.hpp
@@ -0,0 +1,17 @@
+#ifndef LACHUGIN_COUNT_HPP
+#define LACHUGIN_COUNT_HPP
+#include "property.hpp"
+
+namespace lachugin
+{
+  struct Count: SeqProperty
+  {
+    Count();
+  private:
+    void next(int value) override;
+    size_t res() const override;
+    size_t count_;
+  };
+}
+
+#endif
