73c13beb1638e1ce38fbcd1a78844bae57d7f0f5
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-02-15 20:36:51 +0300

    make main.cpp

--- lachugin.mikhail/P1/main.cpp
+++ lachugin.mikhail/P1/main.cpp
@@ -1,106 +1,26 @@
 #include <iostream>
-#include <limits>
-
-namespace lachugin
-{
-  struct SeqProperty
-  {
-    size_t operator()() const
-    {
-      return res();
-    }
-    void operator()(int value)
-    {
-      next(value);
-    }
-    virtual ~SeqProperty() = default;
-
-  private:
-    virtual void next(int value) = 0;
-    virtual size_t res() const = 0;
-  };
-
-  class Count: public SeqProperty
-  {
-    size_t count = 0;
-    void next(int) override
-    {
-      count++;
-    }
-    size_t res() const override
-    {
-      if (count < 2)
-      {
-        throw std::logic_error("insufficient sequence length");
-      }
-      return count;
-    }
-  };
-
-  class AftMax: public SeqProperty
-  {
-    void next(int value) override
-    {
-      if (value < max)
-      {
-        count++;
-      }
-      else
-      {
-        max = value;
-        count = 0;
-      }
-    }
-    size_t res() const override
-    {
-      return count;
-    }
-    size_t count = 0;
-    int max = std::numeric_limits<int>::min();
-  };
-
-  class CntMin: public SeqProperty
-  {
-    void next(int value) override
-    {
-      if (value == min)
-      {
-        count++;
-      }
-      else if (value < min)
-      {
-        min = value;
-        count = 1;
-      }
-    }
-    size_t res() const override
-    {
-      return count;
-    }
-    size_t count = 0;
-    int min = std::numeric_limits<int>::max();
-  };
-
-}
+#include "count.hpp"
+#include "aftMax.hpp"
+#include "cntMin.hpp"
 
 int main()
 {
   lachugin::Count k;
   lachugin::AftMax max;
   lachugin::CntMin min;
-  lachugin::SeqProperty* props[] = {&max, &min, &k};
+  lachugin::SeqProperty* props[] = { &max, &min, &k };
 
   int n = 0;
 
   while (std::cin >> n && n != 0)
   {
-    for (auto* p: props)
+    for (auto* p : props)
     {
       (*p)(n);
     }
   }
 
-  if (std::cin.fail())
+  if (!std::cin && !std::cin.eof())
   {
     std::cerr << "Error: invalid input sequence\n";
     return 1;
@@ -111,10 +31,9 @@ int main()
     k();
     std::cout << max() << "\n" << min();
   }
-  catch (const std::logic_error&)
+  catch (...)
   {
     std::cout << "Error: insufficient sequence length\n";
     return 2;
   }
-
 }
