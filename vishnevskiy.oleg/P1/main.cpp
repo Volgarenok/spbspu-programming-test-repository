d730787c325666dd81eb9aae8e60710a3ae964de
Oleg <n7o.nto@yandex.ru>
2026-02-15 15:59:06 +0300

    Implemented methods of monDec class

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 
 struct grtLss {
-  grtLss();
-  void push(int);
-  size_t result() const;
+  grtLss() : count_(0), first_(0), second_(0), third_(0) {}
+  void operator()(int);
+  size_t operator()() const
+  {
+    return count_;
+  }
  private:
-  size_t count;
-  int first, second, third;
+  size_t count_;
+  int first_, second_, third_;
 };
 
 struct monDec {
-  monDec();
-  void push(int);
-  size_t result() const;
+  monDec() : tempCount_(0), count_(0), curr_(0), prev_(0), f_(0) {}
+  void operator()(int a)
+  {
+    prev_ = curr_;
+    curr_ = a;
+    if (f == 0)
+    {
+      tempCount_ = 1;
+      f = 1;
+    }
+    else
+    {
+      if (tempCount_ > count_)
+      {
+        count_ = tempCount_;
+      }
+      tempCount_ = 0;
+    }
+  }
+  size_t operator()() const
+  {
+    return count_;
+  }
  private:
-  size_t count;
-  int curr, prev;
+  size_t count_, tempCount_;
+  int curr_, prev_, f_;
 };
 
 int main()
@@ -25,15 +48,15 @@ int main()
   monDec m;
   while (std::cin >> el)
   {
-    g.push(el);
-    m.push(el);
+    g(el);
+    m(el);
   }
   if (!std::cin.eof())
   {
     std::cerr << "Input failed\n";
     return 1;
   }
-  std::cout << g.result() << " " << m.result() << "\n";
+  std::cout << g() << " " << m() << "\n";
   return 0;
 }
 
