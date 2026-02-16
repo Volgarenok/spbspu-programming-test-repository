441f4b8a329b23d31536a16b691e49bc0e1b0611
prblmtc11 <prblmtc11@gmail.com>
2026-02-16 23:03:36 +0300

    add findsecondmax 2.0

--- yarmolinskaya.anna/P1/main.cpp
+++ yarmolinskaya.anna/P1/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <limitis>
 
-int main()
-{}
+int findSecondMax(int &first, int &second) {
+  first = std::numeric_limits<int>::min();
+  second = std::numeric_limits<int>::min();
+  int value = 0;
+  bool has_input = false;
+
+  while (std::cin >> value) {
+    if (value == 0) {
+      break;
+    }
+    has_input = true;
+
+    if (value > first) {
+      second = first;
+      first = value;
+    } else if (value > second && value != first) {
+      second = value;
+    }
+  }
