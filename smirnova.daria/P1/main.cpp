ac073ef8fd291e0d74826067eb137ecc6b7f14aa
dassrm <daryasm1rnoff@yandex.ru>
2026-02-16 14:44:36 +0300

    full solution for LOC_MAX

--- smirnova.daria/P1/main.cpp
+++ smirnova.daria/P1/main.cpp
@@ -33,16 +33,62 @@ private:
   bool error_;
 };
 
+class LocalMaxCounter {
+public:
+  LocalMaxCounter():
+    prev_prev_(0),
+    prev_(0),
+    current_(0),
+    count_(0),
+    length_(0)
+  {}
+  
+  void addValue(int value) {
+    if (length_ >= 2) {
+      prev_prev_ = prev_;
+      prev_ = current_;
+      current_ = value;
+      
+      if (prev_ > prev_prev_ && prev_ > current_) {
+        count_++;
+      }
+    } else if (length_ == 1) {
+      prev_ = current_;
+      current_ = value;
+    } else {
+      current_ = value;
+    }
+    
+    length_++;
+  }
+  
+  int getCount() const {
+    return count_;
+  }
+  
+private:
+  int prev_prev_;
+  int prev_;
+  int current_;
+  int count_;
+  int length_;
+};
+
 }
 
 int main() {
   smirnova::InputReader reader;
+  smirnova::LocalMaxCounter counter;
+  
   int value;
+  bool hasElements = false;
   
   while (reader.readNext(value)) {
     if (value == 0) {
       break;
     }
+    hasElements = true;
+    counter.addValue(value);
   }
   
   if (reader.hasError()) {
@@ -50,7 +96,12 @@ int main() {
     return 1;
   }
   
-  std::cout << "0" << std::endl;
+  if (!hasElements) {
+    std::cerr << "Error: Empty sequence" << std::endl;
+    return 2;
+  }
+  
+  std::cout << counter.getCount() << std::endl;
   
   return 0;
 }
