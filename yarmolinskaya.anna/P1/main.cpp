a900110d24a2fb83bf8eb433453fa8231c26ebfd
prblmtc11 <prblmtc11@gmail.com>
2026-02-16 23:07:40 +0300

    add namespace yarmolinskaya

--- yarmolinskaya.anna/P1/main.cpp
+++ yarmolinskaya.anna/P1/main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
-#include <limitis>
+#include <limits>
 
-int findSecondMax(int &first, int &second) {
-  first = std::numeric_limits<int>::min();
-  second = std::numeric_limits<int>::min();
-  int value = 0;
-  bool has_input = false;
+namespace yarmolinskaya {
+  int findSecondMax(int &first, int &second) {
+    first = std::numeric_limits<int>::min();
+    second = std::numeric_limits<int>::min();
+    int value = 0;
+    bool has_input = false;
 
-  while (std::cin >> value) {
-    if (value == 0) {
-      break;
+    while (std::cin >> value) {
+      if (value == 0) {
+        break;
+      }
+      has_input = true;
+
+      if (value > first) {
+        second = first;
+        first = value;
+      } else if (value > second && value != first) {
+        second = value;
+      }
     }
-    has_input = true;
 
-    if (value > first) {
-      second = first;
-      first = value;
-    } else if (value > second && value != first) {
-      second = value;
+    if (!std::cin.eof() && std::cin.fail()) {
+      return 1;
     }
+
+    if (!has_input || second == std::numeric_limits<int>::min()) {
+      return 2;
+    }
+
+    return 0;
   }
 
-  if (!std::cin.eof() && std::cin.fail()) {
-    return 1;
-  }
-
-  if (!has_input || second == std::numeric_limits<int>::min()) {
-    return 2;
-  }
-
-  return 0;
 }
