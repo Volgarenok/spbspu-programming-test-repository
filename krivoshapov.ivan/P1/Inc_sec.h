0c0500ea76d9dd64ce5afdff6809caa6a1472e80
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-17 19:19:03 +0300

    add Inc_sec header and minor CG fix

---
+++ krivoshapov.ivan/P1/Inc_sec.h
@@ -0,0 +1,17 @@
+#include "Itrait.h"
+
+#ifndef INC_SEQ_H
+#define INC_SEQ_H
+
+struct IncSeq : ITrait {
+    IncSeq(): k_(0), prev_(0), n_(0) {}
+private:
+    void upd(int a) override;
+    size_t result() const override;
+    const char *name() const override;
+    size_t k_;
+    int    prev_;
+    size_t n_;
+};
+
+#endif
--- krivoshapov.ivan/P1/Itrait.h
+++ krivoshapov.ivan/P1/Itrait.h
@@ -10,14 +10,14 @@ struct ITrait {
     size_t operator()() const {
         return result();
     }
-    const char * trait() const {
+    const char *trait() const {
         return name();
     }
     virtual ~ITrait() = default;
 private:
     virtual void upd(int) = 0;
     virtual size_t result() const = 0;
-    virtual const char * name() const = 0;
+    virtual const char *name() const = 0;
 };
 
 #endif
