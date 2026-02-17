96bb71f5647e283ce432354ada9deb60f89f97f3
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-17 19:59:47 +0300

    itrait add and fix

--- krivoshapov.ivan/P1/inc_seq.h
+++ krivoshapov.ivan/P1/inc_seq.h
@@ -1,31 +1,19 @@
 #include "itrait.h"
 
-// =========================================================
-// COMMIT 2: "Add IncSeq functor header"
-// =========================================================
-//
-// Пункт 2: каждая характеристика реализует интерфейс
-//          в собственном классе
-//
-// [INC-SEQ] Определить, сколько элементов последовательности
-// больше предыдущего элемента.
-//
-// Пример: 1 2 3 -2 -1 0  =>  3
-//         1 0             =>  0
-//         0               =>  0
-
 #ifndef INC_SEQ_H
 #define INC_SEQ_H
 
-struct IncSeq : ITrait {
-    IncSeq(): k_(0), prev_(0), n_(0) {}
+struct IncSeq : ITrait
+{
+  IncSeq() : k_(0), prev_(0), n_(0) {}
+
 private:
-    void upd(int a) override;
-    size_t result() const override;
-    const char * name() const override;
-    size_t k_;
-    int    prev_;
-    size_t n_;
+  void upd(int a) override;
+  size_t result() const override;
+  const char *name() const override;
+  size_t k_;
+  int prev_;
+  size_t n_;
 };
 
 #endif
