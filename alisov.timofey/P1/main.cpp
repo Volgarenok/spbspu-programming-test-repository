aabba7eb493b1f3996cdc844dee344477ce9eb1f
AlisovTimofey <timofeyalisov@gmail.com>
2026-02-17 10:35:49 +0300

    add throw and a lil more

--- alisov.timofey/P1/main.cpp
+++ alisov.timofey/P1/main.cpp
@@ -1,82 +1,87 @@
 #include <iostream>
-
-struct ITrait
+namespace alisov
 {
-  virtual void operator()(int a) = 0;
-  virtual int operator()() = 0;
-  virtual ~ITrait() = default;
-};
-
-struct DivRem: ITrait
-{
-  DivRem():
-    count_(0),
-    total_(0),
-    prev_(0)
-  {}
-
-  void operator()(int a)
+  struct ITrait
   {
-    total_++;
+    virtual void operator()(int a) = 0;
+    virtual int operator()() const = 0;
+    virtual ~ITrait() = default;
+  };
 
-    if (prev_ != 0 && a % prev_ == 0)
+  struct DivRem: ITrait
+  {
+    DivRem():
+      count_(0),
+      total_(0),
+      prev_(0)
+    {}
+
+    void operator()(int a) override
     {
-      count_++;
+      total_++;
+
+      if (prev_ != 0 && a % prev_ == 0)
+      {
+        count_++;
+      }
+
+      prev_ = a;
     }
 
-    prev_ = a;
-  }
-
-  int operator()()
-  {
-    return count_;
-  }
-
-private:
-  int count_;
-  int total_;
-  int prev_;
-};
-
-struct EvenCount: ITrait
-{
-  EvenCount():
-    current_(0),
-    max_(0)
-  {}
-
-  void operator()(int a)
-  {
-    if (a % 2 == 0)
+    int operator()() const override
     {
-      current_++;
-      if (current_ > max_)
+      if (total_ < 2)
       {
-        max_ = current_;
+        throw std::exception();
+      }
+      return count_;
+    }
+
+  private:
+    size_t count_;
+    int total_;
+    int prev_;
+  };
+
+  struct EvenCount: ITrait
+  {
+    EvenCount():
+      current_(0),
+      max_(0)
+    {}
+
+    void operator()(int a) override
+    {
+      if (a % 2 == 0)
+      {
+        current_++;
+        if (current_ > max_)
+        {
+          max_ = current_;
+        }
+      }
+      else
+      {
+        current_ = 0;
       }
     }
-    else
+
+    int operator()() const override
     {
-      current_ = 0;
+      return max_;
     }
-  }
 
-  int operator()()
-  {
-    return max_;
-  }
-
-private:
-  int current_;
-  int max_;
-};
-
+  private:
+    int current_;
+    int max_;
+  };
+}
 int main()
 {
-  int number = 0;
+  alisov::DivRem divrem;
+  alisov::EvenCount evencnt;
 
-  DivRem divrem;
-  EvenCount evencnt;
+  int number;
   while ((std::cin >> number) && (number != 0))
   {
     divrem(number);
