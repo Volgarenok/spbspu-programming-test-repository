2b5368150ad74850bf29c1eb5c89f0cacd7716fa
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-17 16:22:26 +0300

    add DIV-REM realization

--- krivoshapov.ivan/P1/main.cpp
+++ krivoshapov.ivan/P1/main.cpp
@@ -38,6 +38,30 @@ private:
     size_t n_;
 };
 
+struct DivRem : ITrait {
+    DivRem(): k_(0), prev_(0), n_(0) {}
+    bool valid() const {
+        return n_ >=2;
+    }
+private:
+    void upd(int a) {
+        if(n_ > 0 && prev_ != 0 && a%prev_ == 0) {
+            ++k_;
+        }
+        prev_ = a;
+        ++n_;
+    }
+    size_t result() const {
+        return k_;
+    }
+    const char *name() const {
+        return "div-rem";
+    }
+    size_t k_;
+    int prev_;
+    size_t n_;
+};
+
 int main(){
     int error = 0;
     int a = 0;
