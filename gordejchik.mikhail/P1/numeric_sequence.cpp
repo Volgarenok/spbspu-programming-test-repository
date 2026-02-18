970ee50002dbd0fcf99f7742f61efeede6cb07e3
uncharted-bread <gordeychikmika@gmail.com>
2026-02-19 00:40:37 +0300

    fix CG and numeric_limits

--- gordejchik.mikhail/P1/numeric_sequence.cpp
+++ gordejchik.mikhail/P1/numeric_sequence.cpp
@@ -2,130 +2,116 @@
 #include <iostream>
 #include <limits>
 
-namespace gordejchik
+gordejchik::EvenCount::EvenCount()
 {
-  EvenCount::EvenCount()
+  maxEvenCount = 0;
+  currentEvenCount = 0;
+  totalNumbers = 0;
+}
+
+void gordejchik::EvenCount::update(int number)
+{
+  if (number % 2 == 0)
   {
-    maxEvenCount = 0;
+    ++currentEvenCount;
+    if (currentEvenCount > maxEvenCount)
+    {
+      maxEvenCount = currentEvenCount;
+    }
+  }
+  else
+  {
     currentEvenCount = 0;
-    totalNumbers = 0;
   }
+  ++totalNumbers;
+}
 
-  void EvenCount::update(int number)
+size_t gordejchik::EvenCount::getResult() const
+{
+  return maxEvenCount;
+}
+
+gordejchik::LocalMax::LocalMax()
+{
+  localMaxCount = 0;
+  previousNumber = std::numeric_limits< int >::max();
+  currentNumber = std::numeric_limits< int >::max();
+  nextNumber = std::numeric_limits< int >::max();
+  totalNumbers = 0;
+}
+
+void gordejchik::LocalMax::update(int number)
+{
+  nextNumber = number;
+  if (currentNumber > previousNumber && currentNumber > nextNumber)
   {
-    if (number % 2 == 0)
-    {
-      ++currentEvenCount;
-      if (currentEvenCount > maxEvenCount)
-      {
-        maxEvenCount = currentEvenCount;
-      }
-    }
-    else
-    {
-      currentEvenCount = 0;
-    }
-    ++totalNumbers;
+    ++localMaxCount;
   }
+  previousNumber = currentNumber;
+  currentNumber = nextNumber;
+  ++totalNumbers;
+}
 
-  size_t EvenCount::getResult() const
+size_t gordejchik::LocalMax::getResult() const
+{
+  if (totalNumbers == 0)
   {
-    return maxEvenCount;
+    throw "Error: sequence too short for LOC-MAX";
   }
+  return localMaxCount;
+}
 
-  LocalMax::LocalMax()
+gordejchik::ErrorStatus::ErrorStatus()
+{
+  foundError = false;
+  foundZero = false;
+}
+
+void gordejchik::printError(const char* message, gordejchik::ErrorStatus& error)
+{
+  std::cerr << message << '\n';
+  error.foundError = true;
+}
+
+bool gordejchik::processInput(gordejchik::EvenCount& evenCount, gordejchik::LocalMax& localMax, gordejchik::ErrorStatus& error)
+{
+  int currentNumber = 0;
+  while (true)
   {
-    localMaxCount = 0;
-    previousNumber = std::numeric_limits<int>::min();
-    currentNumber = std::numeric_limits<int>::min();
-    nextNumber = std::numeric_limits<int>::min();
-    totalNumbers = 0;
+    std::cin >> currentNumber;
+    if (std::cin.fail())
+    {
+      printError("Error: sequence contains non-numeric characters", error);
+      return false;
+    }
+    if (std::cin.eof())
+    {
+      printError("Error: sequence doesn't end with a zero", error);
+      return false;
+    }
+    if (currentNumber == 0)
+    {
+      error.foundZero = true;
+      break;
+    }
+    evenCount.update(currentNumber);
+    localMax.update(currentNumber);
   }
+  return true;
+}
 
-  void LocalMax::update(int number)
+void gordejchik::printResults(const gordejchik::EvenCount& evenCount, const gordejchik::LocalMax& localMax, gordejchik::ErrorStatus& error)
+{
+  std::cout << "Results:\n";
+
+  std::cout << "EVN-CNT: " << evenCount.getResult() << '\n';
+
+  try
   {
-    if (totalNumbers == 0)
-    {
-      previousNumber = number;
-    }
-    else if (totalNumbers == 1)
-    {
-      currentNumber = number;
-    }
-    else
-    {
-      nextNumber = number;
-      if (currentNumber > previousNumber && currentNumber > nextNumber)
-      {
-        ++localMaxCount;
-      }
-      previousNumber = currentNumber;
-      currentNumber = nextNumber;
-    }
-    ++totalNumbers;
+    std::cout << "LOC-MAX: " << localMax.getResult() << '\n';
   }
-
-  size_t LocalMax::getResult() const
+  catch (const char* e)
   {
-    if (totalNumbers == 0)
-    {
-      throw "Error: sequence too short for LOC-MAX";
-    }
-    return localMaxCount;
-  }
-
-  ErrorStatus::ErrorStatus()
-  {
-    foundError = false;
-    foundZero = false;
-  }
-
-  void printError(const char* message, ErrorStatus& error)
-  {
-    std::cerr << message << '\n';
-    error.foundError = true;
-  }
-
-  bool processInput(EvenCount& evenCount, LocalMax& localMax, ErrorStatus& error)
-  {
-    int currentNumber = 0;
-    while (true)
-    {
-      std::cin >> currentNumber;
-      if (std::cin.fail())
-      {
-        printError("Error: sequence contains non-numeric characters", error);
-        return false;
-      }
-      if (std::cin.eof())
-      {
-        printError("Error: sequence doesn't end with a zero", error);
-        return false;
-      }
-      if (currentNumber == 0)
-      {
-        error.foundZero = true;
-        break;
-      }
-      evenCount.update(currentNumber);
-      localMax.update(currentNumber);
-    }
-    return true;
-  }
-
-  void printResults(const EvenCount& evenCount, const LocalMax& localMax, ErrorStatus& error)
-  {
-    std::cout << "Results:\n";
-
-    std::cout << "EVN-CNT: " << evenCount.getResult() << '\n';
-
-    try
-    {
-      std::cout << "LOC-MAX: " << localMax.getResult() << '\n';
-    }
-    catch (const char* e)
-    {
-      printError(e, error);
-    }
+    printError(e, error);
   }
 }
