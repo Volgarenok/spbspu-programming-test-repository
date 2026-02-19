e2bc39997e8cc1988c297b9b7a1afe0fbef36cbf
xRonnyn <pashuha.k@gmail.com>
2026-02-20 01:05:50 +0300

    proper text fix

--- karpenkov.pavel/P1/main.cpp
+++ karpenkov.pavel/P1/main.cpp
@@ -8,18 +8,8 @@ int main()
   karpenkov::SignChange signChange;
   karpenkov::ISeqProperty* properties [] = {&aft, &signChange};
   int number;
-  size_t countNum = 0;
   while (std::cin >> number && number != 0){
-    std::cin >> number;
-    if (number == 0) {
-      break;
-    }
-    countNum++;
     for (size_t i = 0; i < 2; i++) {
-      if (countNum == 0 && i == 1) {
-        std::cerr << "Not enough numbers for AFT-MAX" << '\n';
-        return 2;
-      }
       (*properties[i])(number);
     }
   }
