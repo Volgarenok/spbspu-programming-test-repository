88ba809824949271707bfb7610b488efca06bb65
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-02-20 00:06:40 +0300

    add new delete in main

--- lachugin.mikhail/P1/main.cpp
+++ lachugin.mikhail/P1/main.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
 #include "aftMax.hpp"
 #include "cntMin.hpp"
-#include "props.hpp"
 
 int main()
 {
-  lachugin::AftMax max;
-  lachugin::CntMin min;
-  lachugin::SeqProperty* propertys[lachugin::property];
-  lachugin::props(propertys, max, min);
+  lachugin::SeqProperty* propertys[2];
+
+  propertys[0] = new lachugin::AftMax();
+  propertys[1] = new lachugin::CntMin();
 
   int n = 0;
   while (std::cin >> n && n != 0)
   {
-    for (size_t i = 0; i < lachugin::property; ++i)
+    for (size_t i = 0; i < 2; ++i)
     {
-      propertys[i]->operator()(n);
+      (*propertys[i])(n);
     }
   }
 
   if (!std::cin && !std::cin.eof())
   {
     std::cerr << "Error: invalid input sequence\n";
+    for (size_t i = 0; i < 2; ++i)
+    {
+      delete[] propertys[i];
+    }
     return 1;
   }
 
   try
   {
-    for (size_t i = 0; i < lachugin::property; ++i)
+    for (size_t i = 0; i < 2; ++i)
     {
-      std::cout << propertys[i]->operator()() << "\n";
+      std::cout << (*propertys[i])() << "\n";
     }
   }
   catch (const std::logic_error&)
   {
     std::cout << "Error: insufficient sequence length\n";
+    for (size_t i = 0; i < 2; ++i)
+    {
+      delete[] propertys[i];
+    }
     return 2;
+  }
+  for (size_t i = 0; i < 2; ++i)
+  {
+    delete[] propertys[i];
   }
 }
