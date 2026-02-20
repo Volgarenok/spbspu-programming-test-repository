55e50169b8c07f62897e1d2adad2fd6d6a4aef5e
Borislav <91478846+L1sa620@users.noreply.github.com>
2026-02-21 02:07:41 +0300

    Update properties.cpp

--- matveev.borislav/P1/properties.cpp
+++ matveev.borislav/P1/properties.cpp
@@ -3,64 +3,60 @@
 #include <stdexcept>
 #include <limits>
 
-namespace matveev
+void matveev::ISeqProperty::operator()(int value)
 {
-  void ISeqProperty::operator()(int value)
+  next(value);
+}
+
+size_t matveev::ISeqProperty::operator()() const
+{
+  return result();
+}
+
+matveev::CntMaxProperty::CntMaxProperty():
+ max(std::numeric_limits <int>::min()),
+ count(0)
+{
+}
+
+void matveev::CntMaxProperty::next(int value)
+{
+  if (value > max)
   {
-    next(value);
+    max = value;
+    count = 1;
+  }
+  else if (value == max)
+  {
+    ++count;
+  }
+}
+
+size_t matveev::CntMaxProperty::result() const
+{
+  return count;
+}
+
+matveev::DivRemProperty::DivRemProperty(): previous(0), count(0), total(0)
+{
+}
+
+void matveev::DivRemProperty::next(int value)
+{
+  if (total > 0 && previous != 0 && value % previous == 0)
+  {
+    ++count;
   }
 
-  size_t ISeqProperty::operator()() const
-  {
-    return result();
-  }
-  CntMaxProperty::CntMaxProperty(): max(std::numeric_limits<int>::min()),count(0)
-  {
-  }
+  previous = value;
+  ++total;
+}
 
-  void CntMaxProperty::next(int value)
+size_t matveev::DivRemProperty::result() const
+{
+  if (total < 2)
   {
-    if (value > max)
-    {
-      max = value;
-      count = 1;
-    }
-    else if (value == max)
-    {
-      ++count;
-    }
+    throw std::logic_error("Error numbers");
   }
-
-  size_t CntMaxProperty::result() const
-  {
-    return count;
-  }
-  DivRemProperty::DivRemProperty(): previous(0), count(0), total(0)
-  {
-  }
-
-  void DivRemProperty::next(int value)
-  {
-    if (total > 0 && previous != 0 && value % previous == 0)
-    {
-      ++count;
-    }
-
-    previous = value;
-    ++total;
-  }
-
-  size_t DivRemProperty::result() const
-  {
-    if (total < 2)
-    {
-      throw std::logic_error("Error numbers");
-    }
-    return count;
-  }
-  void properties(ISeqProperty* props[property], CntMaxProperty& max, DivRemProperty& div)
-  {
-    props[0] = &max;
-    props[1] = &div;
-  }
+  return count;
 }
