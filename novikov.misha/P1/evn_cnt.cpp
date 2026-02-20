261dcdb0f66796247f6a7e44f05d862c08e5e88a
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-20 14:55:51 +0300

    evn_cnt.cpp complete

--- novikov.misha/P1/evn_cnt.cpp
+++ novikov.misha/P1/evn_cnt.cpp
@@ -2,4 +2,27 @@
 
 namespace novikov
 {
+
+  EvnCnt::EvnCnt():
+    max_(0),
+    cur_(0)
+  {}
+
+  void EvnCnt::operator()(int x)
+  {
+    if (x % 2 == 0) {
+      ++cur_;
+      if (cur_ > max_) {
+        max_ = cur_;
+      }
+    } else {
+      cur_ = 0;
+    }
+  }
+
+  size_t EvnCnt::operator()() const
+  {
+    return static_cast< size_t >(max_);
+  }
+
 }
