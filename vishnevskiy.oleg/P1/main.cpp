d0251f6fc16c4515f1d560df718ef8207312725e
Oleg <n7o.nto@yandex.ru>
2026-02-20 09:46:59 +0300

    Memory leak fix attempt

--- vishnevskiy.oleg/P1/main.cpp
+++ vishnevskiy.oleg/P1/main.cpp
@@ -130,11 +130,15 @@ int main()
   {
     std::cout << traitArray[0]->operator()() << "\n";
     std::cout << traitArray[1]->operator()() << "\n";
+    delete traitArray[0];
+    delete traitArray[1];
     return 0;
   }
   catch (std::logic_error &error)
   {
     std::cout << error.what() << "\n";
+    delete traitArray[0];
+    delete traitArray[1];
     return 2;
   }
 }
