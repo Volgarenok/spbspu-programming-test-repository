a4507c232e5911cdb7af92d7271c30267526d6a1
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-20 14:53:12 +0300

    inc_seq.cpp complete

--- novikov.misha/P1/inc_seq.cpp
+++ novikov.misha/P1/inc_seq.cpp
@@ -1,5 +1,32 @@
 #include "inc_seq.hpp"
+#include <limits>
 
 namespace novikov
 {
+
+  IncSeq::IncSeq():
+    count_(0),
+    total_(0),
+    prev_(std::numeric_limits< int >::max())
+  {}
+
+  void IncSeq::operator()(int x)
+  {
+    if (x > prev_) {
+      ++count_;
+    }
+    ++total_;
+    prev_ = x;
+  }
+
+  size_t IncSeq::operator()() const
+  {
+    return count_;
+  }
+
+  size_t IncSeq::total() const
+  {
+    return total_;
+  }
+
 }
