224c5f9d31c020d4dcd008ab96050375ff39a054
Omar702140 <rnmp4801@gmail.com>
2026-02-21 00:13:39 +0300

    more files

--- terentev.roman/P1/main.cpp
+++ terentev.roman/P1/main.cpp
@@ -1,117 +1,7 @@
+#include "prop_ptr.hpp"
+#include "seq_properties.hpp"
+
 #include <iostream>
-
-namespace terentev {
-
-  struct ISeqProperty {
-    size_t operator()() const
-    {
-      return result();
-    }
-    void operator()(int x)
-    {
-      next(x);
-    }
-    virtual ~ISeqProperty() = default;
-  private:
-    virtual size_t result() const = 0;
-    virtual void next(int x) = 0;
-  };
-
-  struct CountGreaterThanPrev final: ISeqProperty {
-  private:
-    size_t c = 0;
-    int prev = 0;
-
-    size_t result() const override
-    {
-      return c;
-    }
-    void next(int x) override
-    {
-      if (prev == 0) {
-        prev = x;
-        return;
-      }
-      if (x > prev) {
-        ++c;
-      }
-      prev = x;
-    }
-  };
-
-  struct CountMaxOccurrences final: ISeqProperty {
-  private:
-    int max = 0;
-    size_t cnt = 0;
-
-    size_t result() const override
-    {
-      return cnt;
-    }
-    void next(int x) override
-    {
-      if (cnt == 0) {
-        max = x;
-        cnt = 1;
-        return;
-      }
-      if (x > max) {
-        max = x;
-        cnt = 1;
-      } else {
-        if (x == max) {
-          ++cnt;
-        }
-      }
-    }
-  };
-
-  ISeqProperty* get_prop(const std::string& name)
-  {
-    if (name == "gt-prev") {
-      return new CountGreaterThanPrev();
-    }
-    if (name == "max-cnt") {
-      return new CountMaxOccurrences();
-    }
-    throw std::invalid_argument("unknown property: " + name);
-  }
-
-  struct PropPtr {
-    ISeqProperty* p = nullptr;
-
-    explicit PropPtr(ISeqProperty* ptr) : p(ptr)
-    {
-    }
-    ~PropPtr()
-    {
-      delete p;
-    }
-
-    PropPtr(const PropPtr&) = delete;
-    PropPtr& operator=(const PropPtr&) = delete;
-
-    PropPtr(PropPtr&& other) noexcept : p(other.p)
-    {
-      other.p = nullptr;
-    }
-    PropPtr& operator=(PropPtr&& other) noexcept
-    {
-      if (this != &other) {
-        delete p;
-        p = other.p;
-        other.p = nullptr;
-      }
-      return *this;
-    }
-
-    ISeqProperty* operator->() const
-    {
-      return p;
-    }
-  };
-
-}
 
 int main()
 {
@@ -149,7 +39,7 @@ int main()
     std::cerr << "not int\n";
     return 1;
   }
+
   std::cout << p1->operator()() << '\n';
   std::cout << p2->operator()() << '\n';
-  return 0;
 }
