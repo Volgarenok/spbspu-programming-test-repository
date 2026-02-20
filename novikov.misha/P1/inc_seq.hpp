a48671b697b2951a5e4b282859e4c1b0a2d4e20f
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-20 14:45:11 +0300

    inc_seq.hpp complete

--- novikov.misha/P1/inc_seq.hpp
+++ novikov.misha/P1/inc_seq.hpp
@@ -1,11 +1,23 @@
-#indef INC_SEQ_HPP
+#ifndef INC_SEQ_HPP
 #define INC_SEQ_HPP
+
+#include <cstddef>
 
 namespace novikov
 {
 
   class IncSeq
   {
+  public:
+    IncSeq();
+    void operator()(int x);
+    size_t operator()() const;
+    size_t total() const;
+
+  private:
+    size_t count_;
+    size_t total_;
+    int prev_;
   };
 
 }
