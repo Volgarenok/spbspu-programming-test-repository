73b014ae02756deef19d48814d97ea7f6f898647
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-20 14:54:49 +0300

    evn_cnt.hpp complete

--- novikov.misha/P1/evn_cnt.hpp
+++ novikov.misha/P1/evn_cnt.hpp
@@ -1,6 +1,23 @@
-#ifndef EVN_CNT.HPP
-#define EVN_CNT.HPP
+#ifndef EVN_CNT_HPP
+#define EVN_CNT_HPP
+
+#include <cstddef>
 
 namespace novikov
 {
+
+  class EvnCnt
+  {
+  public:
+    EvnCnt();
+    void operator()(int x);
+    size_t operator()() const;
+
+  private:
+    int max_;
+    int cur_;
+  };
+
 }
+
+#endif
