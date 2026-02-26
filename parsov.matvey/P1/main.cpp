85933c4d079b83334229c76db269c0f284a90961
blantality <parsoffgambit@gmail.com>
2026-02-26 19:12:04 +0300

    oop design

--- parsov.matvey/P1/main.cpp
+++ parsov.matvey/P1/main.cpp
@@ -1,38 +1,105 @@
 #include <iostream>
-#include <cstddef>
 
 namespace parsov {
 
-  void MonInc(int value, size_t &max_len, size_t &cur_len, int &prev, bool &is_fst)
+  class IProperty {
+  public:
+    virtual ~IProperty() = default;
+    size_t operator()() const;
+    void operator()(int value);
+  private:
+    virtual size_t result() const = 0;
+    virtual void update(int value) = 0;
+  };
+
+  size_t IProperty::operator()() const
   {
-    if (is_fst) {
-      cur_len = 1;
-      max_len = 1;
-      prev = value;
-      is_fst = false;
+    return result();
+  }
+
+  void IProperty::operator()(int value)
+  {
+    update(value);
+  }
+
+  class MonInc : public IProperty {
+  public:
+    MonInc();
+  private:
+    size_t max_length_;
+    size_t current_length_;
+    int prev_val_;
+    bool is_first_;
+    size_t result() const override;
+    void update(int value) override;
+  };
+
+  MonInc::MonInc():
+    max_length_(0),
+    current_length_(0),
+    prev_val_(0),
+    is_first_(true)
+  {}
+
+  size_t MonInc::result() const
+  {
+    return max_length_;
+  }
+
+  void MonInc::update(int value)
+  {
+    if (is_first_) {
+      current_length_ = 1;
+      max_length_ = 1;
+      prev_val_ = value;
+      is_first_ = false;
     } else {
-      if (value >= prev) {
-        cur_len++;
+      if (value >= prev_val_) {
+        current_length_++;
       } else {
-        cur_len = 1;
+        current_length_ = 1;
       }
-      if (cur_len > max_len) {
-        max_len = cur_len;
+      if (current_length_ > max_length_) {
+        max_length_ = current_length_;
       }
-      prev = value;
+      prev_val_ = value;
     }
   }
 
-  void GrtLss(int value, size_t &count, int &p2, int &p1, size_t &seen)
+  class GrtLss : public IProperty {
+  public:
+    GrtLss();
+  private:
+    size_t count_;
+    int prev2_;
+    int prev1_;
+    size_t elements_seen_;
+    size_t result() const override;
+    void update(int value) override;
+  };
+
+  GrtLss::GrtLss():
+    count_(0),
+    prev2_(0),
+    prev1_(0),
+    elements_seen_(0)
+  {}
+
+  size_t GrtLss::result() const
   {
-    seen++;
-    if (seen >= 3) {
-      if (p2 > p1 && p1 > value) {
-        count++;
+    return count_;
+  }
+
+  void GrtLss::update(int value)
+  {
+    elements_seen_++;
+    if (elements_seen_ >= 3) {
+      if (prev2_ > prev1_ && prev1_ > value) {
+        count_++;
       }
     }
-    p2 = p1;
-    p1 = value;
+    prev2_ = prev1_;
+    prev1_ = value;
   }
 
 }
@@ -41,20 +108,12 @@ int main()
 {
   int err = 0;
   int value = 0;
-
-  size_t max_length = 0;
-  size_t current_length = 0;
-  int prev_val = 0;
-  bool is_first = true;
-
-  size_t count = 0;
-  int prev2 = 0;
-  int prev1 = 0;
-  size_t elements_seen = 0;
+  parsov::MonInc mon_inc;
+  parsov::GrtLss grt_lss;
 
   while (std::cin >> value && value != 0) {
-    parsov::MonInc(value, max_length, current_length, prev_val, is_first);
-    parsov::GrtLss(value, count, prev2, prev1, elements_seen);
+    mon_inc(value);
+    grt_lss(value);
   }
 
   if (!std::cin.eof() && std::cin.fail()) {
@@ -62,22 +121,19 @@ int main()
     return 1;
   }
 
-  bool can_calc_m = true;
-  bool can_calc_g = true;
-
-  if (can_calc_m) {
-    std::cout << max_length << "\n";
-  } else {
-    std::cerr << "Error\n";
+  try {
+    std::cout << mon_inc() << "\n";
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
     if (err == 0) {
       err = 2;
     }
   }
 
-  if (can_calc_g) {
-    std::cout << count << "\n";
-  } else {
-    std::cerr << "Error\n";
+  try {
+    std::cout << grt_lss() << "\n";
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
     if (err == 0) {
       err = 2;
     }
