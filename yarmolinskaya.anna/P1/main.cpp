bdad30aacd78ac05257ee89110b0c7036cd419bd
prblmtc11 <prblmtc11@gmail.com>
2026-03-11 23:36:57 +0300

    added changed files

---
+++ yarmolinskaya.anna/P1/main.cpp
@@ -0,0 +1,20 @@
+#include "io.hpp"
+#include <iostream>
+
+int main()
+{
+  const yarmolinskaya::result_t res = yarmolinskaya::compute(std::cin);
+  if (!res.inputValid) {
+    std::cerr << "Error: input cannot be identified as a valid integer sequence\n";
+    return 1;
+  }
+  int exitCode = 0;
+  if (res.subMaxValid) {
+    std::cout << "SUB-MAX: " << res.subMax << "\n";
+  } else {
+    std::cerr << "Error: SUB-MAX requires at least 2 distinct elements\n";
+    exitCode = 2;
+  }
+  std::cout << "EQL-SEQ: " << res.eqlSeq << "\n";
+  return exitCode;
+}
