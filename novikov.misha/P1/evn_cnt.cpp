34e2b8b15268a182ffde311bcf6a25a6ab195512
Mishaster82 <misha.novikov2007@gmail.com>
2026-03-14 23:48:35 +0300

    fix CG

--- novikov.misha/P1/evn_cnt.cpp
+++ novikov.misha/P1/evn_cnt.cpp
@@ -1,29 +1,29 @@
 #include "evn_cnt.hpp"
 #include <cmath>
-
+ 
 namespace novikov
 {
-
-EvnCnt::EvnCnt():
-max_(0),
-cur_(0)
-{}
-
-void EvnCnt::operator()(int x)
-{
-if (std::abs(x) % 2 == 0) {
-++cur_;
-if (cur_ > max_) {
-max_ = cur_;
-}
-} else {
-cur_ = 0;
-}
-}
-
-size_t EvnCnt::operator()() const
-{
-return max_;
-}
-
+ 
+  EvnCnt::EvnCnt():
+    max_(0),
+    cur_(0)
+  {}
+ 
+  void EvnCnt::operator()(int x)
+  {
+    if (std::abs(x) % 2 == 0) {
+      ++cur_;
+      if (cur_ > max_) {
+        max_ = cur_;
+      }
+    } else {
+      cur_ = 0;
+    }
+  }
+ 
+  size_t EvnCnt::operator()() const
+  {
+    return max_;
+  }
+ 
 }
