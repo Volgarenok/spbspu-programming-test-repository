3eea1ac4061913cbc1d7db1625ded8c0eeb130cb
s-sadovnik <sadovnik.sofi@gmail.com>
2026-03-18 09:25:18 +0300

    Added namespace to constructor, changes as per comments

--- sadovnik.sofi/P1/sub_max.cpp
+++ sadovnik.sofi/P1/sub_max.cpp
@@ -1,40 +1,41 @@
 #include "sub_max.hpp"
 #include <stdexcept>
 
-namespace sadovnik {
+sadovnik::SubMaxCount::SubMaxCount():
+  cnt_(0), 
+  max1_(std::numeric_limits<int>::min()),
+  max2_(std::numeric_limits<int>::min())
+{}
 
-SubMaxCount::SubMaxCount()
-  : cnt_(0),
-    max1_(std::numeric_limits<int>::min()),
-    max2_(std::numeric_limits<int>::min())
+void sadovnik::SubMaxCount::operator()(int x)
 {
-}
-
-void SubMaxCount::operator()(int x)
-{
-  if (cnt_ == 0) {
+  if (cnt_ == 0)
+  {
     max1_ = x;
-  } else if (x > max1_) {
+  }
+  else if (x > max1_)
+  {
     max2_ = max1_;
     max1_ = x;
-  } else if (x > max2_) {
+  }
+  else if (x > max2_)
+  {
     max2_ = x;
   }
   ++cnt_;
 }
 
-int SubMaxCount::operator()() const
+int sadovnik::SubMaxCount::operator()() const
 {
-  if (!valid()) {
-    throw std::logic_error("Not enough numbers");
+  if (!valid())
+  {
+    throw std::logic_error("not enough numbers");
   }
   return max2_;
 }
 
-bool SubMaxCount::valid() const
+bool sadovnik::SubMaxCount::valid() const
 {
   return cnt_ >= 2;
-}
-
 }
 
