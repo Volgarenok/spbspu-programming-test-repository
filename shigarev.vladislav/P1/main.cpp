ac54d83653e55867ad11c4d5ba7abe18fe8c6b54
GodOfBruh <virysizer@gmail.com>
2026-03-18 14:56:59 +0300

    Mew main.cpp was added.

--- shigarev.vladislav/P1/main.cpp
+++ shigarev.vladislav/P1/main.cpp
@@ -1,21 +1,31 @@
-#include <cerrno>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
-namespace shigarev {
-
-}
+#include "DivRem.hpp"
+#include "SgnChg.hpp"
 
 int main()
 {
-    shigarev::SgnChg sgnChg;
-    shigarev::DivRem divRem;
+  shigarev::SgnChg sgnChg;
+  shigarev::DivRem divRem;
 
-    if (!shigarev::processInput(sgnChg, divRem)) {
-        return 1;
-    }
+  for (int val = 0; std::cin >> val && val != 0;) {
+    sgnChg.process(val);
+    divRem.process(val);
+  }
+  if (std::cin.fail() && !std::cin.eof()) {
+    std::cerr << "Error: invalid input\n";
+    return 1;
+  }
 
-    return shigarev::printResults(sgnChg, divRem);
+  std::cout << sgnChg.count() << "\n";
+
+  try {
+    std::cout << divRem.count() << "\n";
+  } catch (const std::logic_error & Description) {
+    std::cerr << "Error: " << Description.what() << "\n";
+    return 2;
+  }
+
+  return 0;
 }
