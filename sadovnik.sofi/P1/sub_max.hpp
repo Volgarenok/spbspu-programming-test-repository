73aa428c810d53d3271a0f6343a251d1af65bb73
s-sadovnik <sadovnik.sofi@gmail.com>
2026-03-18 09:16:19 +0300

    Editing header guard, fix space

--- sadovnik.sofi/P1/sub_max.hpp
+++ sadovnik.sofi/P1/sub_max.hpp
@@ -1,24 +1,23 @@
-#ifndef SUB_MAX_hpp
-#define SUB_MAX_hpp
+#ifndef SUB_MAX_HPP
+#define SUB_MAX_HPP
 
 #include <cstddef>
 #include <limits>
 
-namespace sadovnik {
+namespace sadovnik
+{
+  class SubMaxCount {
+  public:
+    SubMaxCount();
+    void operator()(int x);
+    int operator()() const;
 
-class SubMaxCount {
-public:
-  SubMaxCount();
-  void operator()(int x);
-  int operator()() const;
-
-private:
-  bool valid() const;
-  size_t cnt_;
-  int max1_;
-  int max2_;
-};
-
+  private:
+    size_t cnt_;
+    int max1_;
+    int max2_;
+    bool valid() const;
+  };
 }
 
 #endif
