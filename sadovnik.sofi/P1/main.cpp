3ef5ed9f769a263f0a844b5d8c4e519b7b59d38c
s-sadovnik <sadovnik.sofi@gmail.com>
2026-03-18 09:33:26 +0300

    CG 7, 8, 3; changes as per comments

--- sadovnik.sofi/P1/main.cpp
+++ sadovnik.sofi/P1/main.cpp
@@ -5,39 +5,42 @@
 
 int main()
 {
-  using std::cin;
-  using std::cout;
-  using std::cerr;
-
-  int err = 0;
-  int x;
-
   sadovnik::SubMaxCount sub;
   sadovnik::AftMaxCount aft;
 
-  while (cin >> x) {
-    if (x == 0) break;
+    int x;
+
+  while (std::cin >> x)
+  {
+    if (x == 0) {
+        break;
+    }
     sub(x);
     aft(x);
   }
 
-  if (cin.fail() && !cin.eof()) {
-    cerr << "bad input\n";
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "bad input\n";
     return 1;
   }
-  if (cin.eof()) {
-    cerr << "no zero\n";
+  if (std::cin.eof())
+  {
+    std::cerr << "no zero\n";
     return 1;
   }
 
-  try {
-    cout << sub() << '\n';
+    int err = 0;
+
+  try
+  {
+    std::cout << sub() << '\n';
   } catch (const std::exception&) {
-    cerr << "not enough numbers\n";
+    std::cerr << "not enough numbers\n";
     err = 2;
   }
 
-  cout << aft() << '\n';
+  std::cout << aft() << '\n';
 
   return err;
 }
