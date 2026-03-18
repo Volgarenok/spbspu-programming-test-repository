3fd63ee2196b495f67a285e34189b108dd88fd97
blantality <parsoffgambit@gmail.com>
2026-03-18 12:53:13 +0300

    base class pointers

--- parsov.matvey/P1/main.cpp
+++ parsov.matvey/P1/main.cpp
@@ -7,35 +7,37 @@ int main()
 {
   int err = 0;
   int value = 0;
-  parsov::MonInc mon_inc;
-  parsov::GrtLss grt_lss;
+  parsov::IProperty * props[2];
+  props[0] = new parsov::MonInc();
+  props[1] = new parsov::GrtLss();
 
   while (std::cin >> value && value != 0) {
-    mon_inc(value);
-    grt_lss(value);
+    for (size_t i = 0; i < 2; ++i) {
+      (*props[i])(value);
+    }
   }
 
   if (!std::cin.eof() && std::cin.fail()) {
     std::cerr << "Bad input\n";
+    for (size_t i = 0; i < 2; ++i) {
+      delete props[i];
+    }
     return 1;
   }
 
-  try {
-    std::cout << mon_inc() << "\n";
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << "\n";
-    if (err == 0) {
-      err = 2;
+  for (size_t i = 0; i < 2; ++i) {
+    try {
+      std::cout << (*props[i])() << "\n";
+    } catch (const std::exception & e) {
+      std::cerr << e.what() << "\n";
+      if (err == 0) {
+        err = 2;
+      }
     }
   }
 
-  try {
-    std::cout << grt_lss() << "\n";
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << "\n";
-    if (err == 0) {
-      err = 2;
-    }
+  for (size_t i = 0; i < 2; ++i) {
+    delete props[i];
   }
 
   return err;
