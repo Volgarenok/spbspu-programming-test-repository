1428400eb2f63c11012f206f50a36a17b0c7e013
blantality <parsoffgambit@gmail.com>
2026-03-18 13:19:47 +0300

    memory safety

--- parsov.matvey/P1/main.cpp
+++ parsov.matvey/P1/main.cpp
@@ -7,9 +7,16 @@ int main()
 {
   int err = 0;
   int value = 0;
-  parsov::IProperty * props[2];
-  props[0] = new parsov::MonInc();
-  props[1] = new parsov::GrtLss();
+  parsov::IProperty * props[2] = {nullptr, nullptr};
+
+  try {
+    props[0] = new parsov::MonInc();
+    props[1] = new parsov::GrtLss();
+  } catch (const std::bad_alloc & e) {
+    std::cerr << "Memory allocation failed\n";
+    delete props[0];
+    return 1;
+  }
 
   while (std::cin >> value && value != 0) {
     for (size_t i = 0; i < 2; ++i) {
