5f6d8251a5e5e1d2a2a2803b86a22990e58e0153
s-sadovnik <sadovnik.sofi@gmail.com>
2026-03-22 19:29:49 +0300

    change code in main.cpp

--- sadovnik.sofi/P1/main.cpp
+++ sadovnik.sofi/P1/main.cpp
@@ -5,17 +5,9 @@
 
 int main()
 {
-  using std::cin;
-  using std::cout;
-  using std::cerr;
-
-  int err = 0;
-  int x;
-
   sadovnik::SubMaxCount sub;
   sadovnik::AftMaxCount aft;
 
-<<<<<<< HEAD
   int x;
 
   while (std::cin >> x)
@@ -24,24 +16,21 @@ int main()
     {
       break;
     }
-=======
-  while (cin >> x) {
-    if (x == 0) break;
->>>>>>> parent of 3ef5ed9 (CG 7, 8, 3; changes as per comments)
     sub(x);
     aft(x);
   }
 
-  if (cin.fail() && !cin.eof()) {
-    cerr << "bad input\n";
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "bad input\n";
     return 1;
   }
-  if (cin.eof()) {
-    cerr << "no zero\n";
+  if (std::cin.eof())
+  {
+    std::cerr << "no zero\n";
     return 1;
   }
 
-<<<<<<< HEAD
   int err = 0;
 
   try
@@ -51,16 +40,10 @@ int main()
   catch (const std::exception&)
   {
     std::cerr << "not enough numbers\n";
-=======
-  try {
-    cout << sub() << '\n';
-  } catch (const std::exception&) {
-    cerr << "not enough numbers\n";
->>>>>>> parent of 3ef5ed9 (CG 7, 8, 3; changes as per comments)
     err = 2;
   }
 
-  cout << aft() << '\n';
+  std::cout << aft() << '\n';
 
   return err;
 }
